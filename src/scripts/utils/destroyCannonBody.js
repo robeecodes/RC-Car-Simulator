49977f7afd6591ad12f99533dde4c21375841caa
Robin <97387960+robeecodes@users.noreply.github.com>
2025-01-16 23:31:35 +0000

    docs(destroyCannonBody.js): update comments

--- src/scripts/utils/destroyCannonBody.js
+++ src/scripts/utils/destroyCannonBody.js
@@ -1,3 +1,9 @@
+/**
+ * Destroy a cannon-es body
+ * @param {CANNON.Body} body The physics body to destroy
+ * @param {CANNON.World} physicsWorld The physics world the body is in
+ * @public
+ */
 export default function destroyCannonBody(body, physicsWorld) {
     if (body && physicsWorld) {
         physicsWorld.removeBody(body);
