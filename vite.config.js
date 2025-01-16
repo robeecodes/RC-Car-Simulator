32883bec699728a7808efa18cc0628acfb389898
Robin <97387960+robeecodes@users.noreply.github.com>
2025-01-16 20:27:45 +0000

    fix(vite.config.js): update config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import restart from 'vite-plugin-restart';
 import {defineConfig} from 'vite'
 
 export default defineConfig({
-    base: '',
+    base: '/rc-car-simulator/',
     root: 'src/', // Sources files
     publicDir: '../static/', // Path from "root" to static assets
     server:
